8fd8977ba50c45c07401f05e32dffe9e273136be
zhaojun <873019219@qq.com>
2023-11-05 18:01:02 +0800

    替换模板

--- src/template.ts
+++ src/template.ts
@@ -1,129 +1,109 @@
 export const htmlTemplate = `
+
 <!DOCTYPE html>
-<html>
-
+<html lang="zh-Hans">
 <head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="author" content="Jetsung Chan">
-	<title>{{ title }}</title>
-	<style>
-		* {
-			box-sizing: border-box;
-		}
-
-		body {
-			margin: 0;
-			padding: 0;
-			background-color: #f4f4f4;
-			font-family: Arial, Helvetica, sans-serif;
-		}
-
-		.container {
-			position: absolute;
-			top: calc(50% - 10rem);
-			left: 50%;
-			transform: translate(-50%, -50%);
-			width: 96%;
-			max-width: 640px;
-		}
-
-		.form {
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			align-items: center;
-			border: 1px solid #ddd;
-			border-radius: 10px;
-			padding: 20px;
-			background-color: #fff;
-			box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-		}
-
-		.form h2 {
-			margin-bottom: 20px;
-			text-align: center;
-		}
-
-		.form input[type="text"] {
-			width: 100%;
-			padding: 10px;
-			margin-bottom: 20px;
-			border: 1px solid #ddd;
-			border-radius: 5px;
-			font-size: 16px;
-			outline: none;
-		}
-
-		.form button[type="submit"] {
-			padding: 10px 20px;
-			background-color: #4caf50;
-			color: #fff;
-			border: none;
-			border-radius: 5px;
-			font-size: 16px;
-			cursor: pointer;
-		}
-
-		.form button[type="submit"]:hover {
-			background-color: #3e8e41;
-		}
-
-		.copyright {
-			position: absolute;
-			bottom: 0;
-			left: 50%;
-			transform: translateX(-50%);
-			margin-bottom: 10px;
-			font-size: 14px;
-			color: #999;
-		}
-
-		.a-1 {
-			font-weight: bolder;
-		}
-
-		.p-1 a:hover,
-		.p-1 a:link,
-		.p-1 a:visited,
-		.p-1 a:active {
-			text-decoration: none;
-		}
-
-		.p-1 a:hover {
-			color: #055905;
-		}
-	</style>
-	<script>
-		function toSubmit (e) {
-			e.preventDefault()
-			const fileUrlInput = downloadForm.querySelector('input[name="fileUrl"]')
-			// 编码 fileUrl
-			const encodedFileUrl = encodeURIComponent(fileUrlInput.value);
-			const reqUrl = location.origin + '/' + encodedFileUrl
-
-			window.open(reqUrl)
-			// location.href = reqUrl
-			return false
-		}
-	</script>
+ <meta charset="UTF-8">
+ <title>{{ title }}</title>
+ <style>
+  /* 设置页面样式 */
+  body {
+   background-color: #f7f7f7;
+   font-family: Arial, sans-serif;
+   margin: 0;
+   padding: 0;
+  }
+  .container {
+   max-width: 600px;
+   margin: 0 auto;
+   padding: 20px;
+  }
+  h1 {
+   font-size: 36px;
+   margin-bottom: 30px;
+   text-align: center;
+  }
+  input[type="text"] {
+   padding: 10px;
+   font-size: 16px;
+   border-radius: 4px;
+   border: none;
+   width: 80%;
+   margin-right: 10px;
+  }
+  .btn {
+   padding: 10px 20px;
+   font-size: 16px;
+   border-radius: 4px;
+   border: none;
+   background-color: #007bff;
+   color: #fff;
+   cursor: pointer;
+  }
+  .btn:hover {
+   background-color: #0062cc;
+  }
+  .footer {
+   background-color: #444;
+   color: #fff;
+   padding: 10px;
+   text-align: center;
+   position: fixed;
+   bottom: 0;
+   left: 0;
+   width: 100%;
+  }
+  .note {
+   margin-top: 30px;
+   font-size: 14px;
+   line-height: 1.5;
+   color: #8c8b8b;
+  }
+  .note pre {
+   background-color: #f7f7f7;
+   margin: 10px 0;
+  }
+ </style>
 </head>
-
 <body>
-	<div class="container">
-		<form class="form" id="downloadForm" method="GET" onsubmit="toSubmit(event)">
-			<h2>{{ title }}</h2>
-			<input type="text" name="fileUrl" placeholder="请输入文件下载地址" />
-			<br>
-			<button type="submit">下载</button>
-		</form>
-	</div>
-	<div class="copyright">
-		<p class="p-1">
-			© 2023 Base Cloudflare Workers | Powered by <a class="a-1" href="https://blog.jetsung.com" target="_black">Jetsung</a> | Project <a class="a-1" href="https://jihulab.com/devdo/worker-filetas" target="_black">worker-filetas</a>
-		</p>
-	</div>
-</body>
+<div class="container">
+ <h1>文件下载</h1>
+ <form>
+  <input type="text" autofocus placeholder="输入 Github 链接">
+  <button class="btn" type="submit">下载</button>
+ </form>
 
+ <div class="note">
+  <h2>PS:</h2>
+  <p>GitHub 文件链接带不带协议头都可以，支持 release、archive 以及文件，右键复制出来的链接都是符合标准的。</p>
+  <p>注意，不支持项目文件夹</p>
+  <p>支持非 GitHub 的文件下载、图片加速（不支持表单提交方式）</p>
+  <h3>输入合法示例：</h3>
+  <pre>分支源码：https://github.com/repo/project/archive/master.zip</pre>
+  <pre>tag 源码：https://github.com/repo/project/archive/v0.1.0.tar.gz</pre>
+  <pre>分支文件：https://github.com/repo/project/blob/master/filename</pre>
+  <pre>tag 文件：https://github.com/repo/project/releases/download/v0.1.0/example.zip</pre>
+  <pre>非 GitHub 图片加速显示：https://gh.zhaojun.im/https://xxx.com/logo.png</pre>
+  <pre>非 GitHub 文件加速下载：https://gh.zhaojun.im/https://xxx.com/example.zip</pre>
+ </div>
+</div>
+
+
+
+<div class="footer">
+ 由 Cloudflare Workers 支持
+</div>
+
+<script>
+ const form = document.querySelector('form');
+ form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const input = document.querySelector('input[type="text"]');
+  const url = input.value.trim();
+  const origin = window.location.origin;
+  window.location.href = origin + '/' + url;
+ });
+</script>
+</body>
 </html>
 `
